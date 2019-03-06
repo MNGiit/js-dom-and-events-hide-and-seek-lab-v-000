6ebdce9140afdfe8ca707cdadcb7d8328923a661
Mohammed Noori <kmanoorib@gmail.com>
2019-03-06 21:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let pull = document.querySelector("#grand-node").lastChild;
+  let pull = document.getElementById("grand-node").innerText;
  
   return pull;
 }
