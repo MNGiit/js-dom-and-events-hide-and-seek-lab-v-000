072c35e1ac7ff81e2733bea60b65a43a51d45fcc
Mohammed Noori <kmanoorib@gmail.com>
2019-03-06 22:49:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,13 +32,3 @@ function increaseRankBy(n) {
   }
 }
 
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list');
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children;
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-    }
-  }
